af4458560ce704148d4b971498d75d81fb60de80
Gor Mitanshu <gor.mitanshu3@gmail.com>
2023-05-17 12:07:36 +0530

    Updated 17 May 12:07

--- client/src/Layout/Navbar.jsx
+++ client/src/Layout/Navbar.jsx
@@ -1,8 +1,19 @@
 import { AppBar, Grid, Toolbar, Typography } from "@mui/material";
-import React, { useState } from "react";
+import React, {
+  // useEffect,
+  useState,
+} from "react";
 import MenuIcon from "@mui/icons-material/Menu";
+// import AdjustIcon from "@mui/icons-material/Adjust";
 
 const NavBar = (props) => {
+  // const [role, setRole] = useState();
+  // useEffect(() => {
+  //   const token = JSON.parse(localStorage.getItem("auth")).result.token;
+  //   const data = JSON.parse(atob(token.split(".")[1])).admin;
+  //   setRole(data.role);
+  // }, []);
+
   const [isOpen, setOpen] = useState(false);
   const toggleSidebar = () => {
     setOpen(!isOpen);
@@ -26,12 +37,38 @@ const NavBar = (props) => {
               className="toggle-btn"
               sx={{ cursor: "pointer", paddingRight: "16px", fontSize: "40px" }}
             />
+
             <Typography
               variant="h5"
               style={{ margin: "4px", fontWeight: "bold" }}
             >
               Lead Management System
             </Typography>
+
+            {/* <Grid
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                width: "100%",
+              }}
+            >
+              <Typography
+                variant="h6"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  margin: "4px",
+                  color: "green",
+                  fontWeight: "bold",
+                  fontSize: "15px",
+                }}
+              >
+                <AdjustIcon fontSize="small" />
+                {role === "admin" ? <p>ADMIN</p> : <p>EMPLOYEE</p>}
+              </Typography>
+            </Grid> */}
           </Toolbar>
         </AppBar>
       </Grid>
