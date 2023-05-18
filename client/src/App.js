049dc3c9e085a4413507c1ac6586e7a8cbba1069
Gor Mitanshu <gor.mitanshu3@gmail.com>
2023-05-18 11:48:25 +0530

    Updated 18 May 11:48

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
